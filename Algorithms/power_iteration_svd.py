443a429c3cf4062e063fcbdd603e8335a2af7f5f
root <root@LAPTOP-7VIMTM7H.localdomain>
2025-12-02 16:52:44 -0700

    added subspace iteration

--- Algorithms/power_iteration_svd.py
+++ Algorithms/power_iteration_svd.py
@@ -3,13 +3,14 @@ from numpy.linalg import qr
 from numpy.random import randn
 from numpy.typing import NDArray
 
-def random_range_finder(A : NDArray, j : int):
+def random_range_finder(A : NDArray, j : int) -> NDArray:
     ''' Given an mxn matrix A and an integer j, this scheme computes an mxj
         orthonormal matrix Q whose range approximates the range of A.
 
         Parameters:
             A (ndarray) : mxn matrix
             j (int) : the desired number of columns in Q
+
         Returns:
             Q (ndarray) : mxj matrix with range(Q) approximately equal to range(A)
     '''
@@ -20,6 +21,39 @@ def random_range_finder(A : NDArray, j : int):
     Q, R = qr(Y)          # Construct the QR decomposition Y = QR
 
     return Q
+
+
+def random_subspace_iter(A : NDArray, j : int, q : int) -> NDArray:
+    ''' Given an mxn matrix A and integers j and q, this algorithm computes
+        an mxj orthonormal matrix Q whose range approximates the range of A.
+
+            Parameters:
+                A (ndarray) : mxn matrix
+                j (int) : specifies the desired number of columns in Q 
+                q (int) : specifies desired number of iterations to perform
+
+            Returns:
+                Q (ndarray) : mxj matrix with range(Q) approximately equal to range(A)
+    '''
+    m, n = A.shape          # Get dimensions of A
+    W = randn(n, j)         # Draw an nxj standard Gaussian matrix W
+
+    # Form Y0 = AW and compute its QR factorization Y0 = Q0*R0
+    Y_old = A @ W 
+    Q_old, R_old = qr(Y_old)
+
+    # iterate from 1 to q
+    for _ in range(1, q+1):
+        Y_approx = A.conj() @ Q_old
+        Q_approx, R_approx = qr(Y_approx)
+        Y_new = A @ Q_approx 
+        Q_new, R_new = qr(Y_new)
+
+        # update values
+        Q_old = Q_new.copy()
+    
+    # set Q = Qq
+    return Q_new
 
 def random_svd_power_iter(A : NDArray, k : int, q : int = 1) -> NDArray:
     ''' Use Randomized SVD with power iteration to compute the SVD of the matrix A.
