f94c31042274c8a91197b5c62c01216ff2f968c7
lftolman <lftolman@byu.edu>
2025-12-02 17:14:42 -0700

    clean up svd

--- Algorithms/random_svd.py
+++ Algorithms/random_svd.py
@@ -1,22 +1,5 @@
 import numpy as np
 
-def random_range_finder(A : NDArray, j : int):
-    ''' Given an mxn matrix A and an integer j, this scheme computes an mxj
-        orthonormal matrix Q whose range approximates the range of A.
-
-        Parameters:
-            A (ndarray) : mxn matrix
-            j (int) : the desired number of columns in Q
-        Returns:
-            Q (ndarray) : mxj matrix with range(Q) approximately equal to range(A)
-    '''
-    m, n = A.shape        # Get the dimensions of A
-
-    W = randn(n, j)       # Draw an nxj Gaussian random matrix W 
-    Y = A @ W             # Form the mxj matrix Y = AW 
-    Q, R = qr(Y)          # Construct the QR decomposition Y = QR
-
-    return Q
 
 def randomized_svd(A, k, q = 0, range_method = 'qr'):
     '''Compute the approximate randomized SVD of matrix A using either
@@ -26,7 +9,9 @@ def randomized_svd(A, k, q = 0, range_method = 'qr'):
             A (ndarray) : mxn input matrix
             k (int) : target rank
             q (int) : number of power iterations (default is 0)
-            range_method (str) : method to compute the range ('qr' or 'randomized') 
+            range_method (str) : method to compute the range ('qr' or 'subspace_iter')
+                qr : use the basic numpy randomized range finder
+                subspace_iter : use the randomized subspace iteration method (Algorithm 4.4.2)
         Returns:
             U (ndarray) : mxk matrix of left singular vectors
             S (ndarray) : vector of singular values
@@ -39,8 +24,8 @@ def randomized_svd(A, k, q = 0, range_method = 'qr'):
     Y = A @ Ω
     if range_method == 'qr':
         Q,_ = np.linalg.qr(Y)
-    if range_method == 'randomized':
-        Q = randomized_range_finder(A, k)
+    elif range_method == 'subspace_iter':
+        Q = random_subspace_iter(A, 2*k, q)
     B = Q.T @ A
     U, S, Vt = np.linalg.svd(B, full_matrices = False)
     U = Q @ U
